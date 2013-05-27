25b341841c013ea7d71eb81cf34c15164c0dbfc9
andrewbryk <ajbryk@gmail.com>
2013-05-27 11:44:08 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rentapool::Application.config.secret_key_base = '32b5e733915c85564160a81aa868b72e154fe41b04982ac48cdd2f717cf845bcb4f5e6fdc533ab45ca642bd0d89ce1bda282406478ce3324a823b3157cc1bedb'
